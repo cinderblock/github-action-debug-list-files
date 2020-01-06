48bb2c5206a7fce80f6d0ae7d0d216372a30385a
Cameron Tacklind <cameron@tacklind.com>
2020-01-05 22:52:31 -0800

    Add test subgroup

--- src/utils/printList.ts
+++ src/utils/printList.ts
@@ -16,5 +16,9 @@ export async function printList({ debug, list }: Options): Promise<void> {
 
   debug(`Listing all files in list`);
 
+  core.startGroup('test subgroup');
+  console.log('woot!');
+  core.endGroup();
+
   console.log(list);
 }
